c5cfa91e947fa5eee5b70a260a43244a01dfd9c9
Divy <divya.zoom12@gmail.com>
2026-01-21 15:10:30 +0530

    left nav part

--- src/components/Nav.jsx
+++ src/components/Nav.jsx
@@ -7,7 +7,23 @@ const Nav = () => {
         <nav>
             <div className="left">
                 <div className="apple-icon">
-                    <img src="" alt="" />
+                    <img src="./navbar-icons/apple.svg" alt="" />
+                </div>
+
+                <div className="nav-item">
+                    <p>Divy Jain</p>
+                </div>
+
+                <div className="nav-item">
+                    <p>File</p>
+                </div>
+
+                <div className="nav-item">
+                    <p>Window</p>
+                </div>
+
+                <div className="nav-item">
+                    <p>Terminal</p>
                 </div>
             </div>
             <div className="right"></div>
